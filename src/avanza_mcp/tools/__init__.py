423002cbed832fe17a5870ab22ff1e05b8f9c3f8
LurigeLars <201361928+LurigeLars@users.noreply.github.com>
2026-09-22 00:10:43 +0200

    feat: register leveraged screen tool

--- src/avanza_mcp/tools/__init__.py
+++ src/avanza_mcp/tools/__init__.py
@@ -6,6 +6,7 @@ from . import etfs  # noqa: F401
 from . import funds  # noqa: F401
 from . import futures_forwards  # noqa: F401
 from . import instrument_data  # noqa: F401
+from . import leveraged  # noqa: F401
 from . import market_data  # noqa: F401
 from . import search  # noqa: F401
 from . import warrants  # noqa: F401
@@ -16,6 +17,7 @@ __all__ = [
     "funds",
     "futures_forwards",
     "instrument_data",
+    "leveraged",
     "market_data",
     "search",
     "warrants",
